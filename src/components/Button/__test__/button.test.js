0542a45471897b2dd077c776ad69a20739782200
Samuel Padua <samuel.faria@luizalabs.com>
2019-06-03 12:01:22 -0300

    add Button component

---
+++ src/components/Button/__test__/button.test.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+import Button from '../index'
+import { colors } from '../../../constants'
+import { darken } from '../../../utils/LightenDarkenColor'
+
+describe('Button', () => {
+  describe('default', () => {
+    const tree = renderer
+      .create(<Button>Default</Button>)
+      .toJSON()
+
+    it('should render Button', () => {
+      expect(tree).toMatchSnapshot()
+    })
+  
+    it('should change apply dark color when :hover', () => {
+      expect(tree).toHaveStyleRule('background', darken(colors.default, 10), {
+        modifier: ':hover',
+      })
+    })
+
+    it('should change apply dark color when :active', () => {
+      expect(tree).toHaveStyleRule('background', darken(colors.default, 20), {
+        modifier: ':active',
+      })
+    })
+  })
+
+  describe('error', () => {
+    const tree = renderer
+      .create(<Button color="error">Default</Button>)
+      .toJSON()
+
+    it('should render Button with color error', () => {
+      expect(tree).toMatchSnapshot()
+    })
+  
+    it('should change apply dark 10% color when :hover', () => {
+      expect(tree).toHaveStyleRule('background', darken(colors.error, 10), {
+        modifier: ':hover',
+      })
+    })
+
+    it('should change apply dark 20% color when :active', () => {
+      expect(tree).toHaveStyleRule('background', darken(colors.error, 20), {
+        modifier: ':active',
+      })
+    })
+  })
+})
---
+++ src/components/Button/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { darken } from '../../utils/LightenDarkenColor'
+import { colors } from '../../constants'
+
+const ButtonStyled = styled.button`
+  width: 100%;
+  padding: 6px;
+  border: 0;
+  font: inherit;
+  height: 3em;
+  border-radius: 4px;
+  background: ${props => colors[props.color]};
+  box-shadow: none !important;
+  outline: 0;
+  color: white;
+
+  &:hover {
+    background: ${props => darken(colors[props.color], 10)};
+  }
+
+  &:active {
+    background: ${props => darken(colors[props.color], 20)};
+  }
+`
+
+function Button (props) {
+  const { children } = props
+
+  return (
+    <ButtonStyled { ...props }>
+      { children }
+    </ButtonStyled>
+  )
+}
+
+Button.propTypes = {
+  color: PropTypes.oneOf(['default', 'success', 'warning', 'error'])
+}
+
+Button.defaultProps = {
+  color: 'default'
+}
+
+export default Button
